c53767a6e4923eb9dc8b54d0c9cc9d5899478bf0
Taylor Sweetman <taylor@tactable.io>
2022-01-21 12:00:08 -0500

    more work on formatter

--- src/formatters/sqlalchemy_formatter.py
+++ src/formatters/sqlalchemy_formatter.py
@@ -2,12 +2,35 @@ from typing import List
 
 from src.types import ColumnSchema, Mode
 
-
-def construct_sqlalchemy_output(columns: List[ColumnSchema], mode: Mode, table_name: str = "TODO") -> str:
-    result = "from sqlalchemy import Column\n\n"
-    result += f"class {table_name}(Base):\n"
+def construct_sqlalchemy_output(columns: List[ColumnSchema], mode: Mode, table_name: str = "TODO", base_name: str = "Base") -> str:
+    result = construct_imports(columns, mode)
+    result += f"class {table_name}({base_name}):\n"
     result += f"    __tablename__ = \"{table_name}\"\n\n"
     for col in columns:
-        result += f"    {col.name} = Column({col.type})\n"
+        result += f"    {col.name} = Column({type_mapper(col.type)})\n"
 
     return result
+
+def construct_imports(columns: List[ColumnSchema], mode: Mode):
+    result = "from sqlalchemy import Column"
+    sqlalchemy_types = {type_mapper(col.type) for col in columns}
+
+    for type in sqlalchemy_types:
+        result += f", {type}"
+
+    result += "\n\n"
+    return result
+
+def type_mapper(type: str) -> str:
+    if type == "int":
+        return "Integer"
+    elif type == "text":
+        return "TEXT"
+    elif type == "bool":
+        return "Boolean"
+    elif type == "float":
+        return "Float"
+    elif type == "datetime":
+        return "DateTime"
+    else:
+        return type
